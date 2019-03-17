866f1116397c049694c60f65629b0aef2f08120a
abishek.d <abisheksrv@gmail.com>
2019-03-17 16:54:12 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import carousel1 from "./Images/carousel1.png"
+import carousel2 from "./Images/carousel2.png"
+import carousel3 from "./Images/carousel3.png"
+import carousel4 from "./Images/carousel4.png"
 import './App.css';
 
+const carousels = [carousel4, carousel1, carousel2, carousel3]
 class App extends Component {
+  state = {
+    index: 0,
+  }
+
+  componentDidMount() {
+    setInterval(this.slider, 3000)
+  }
+
+  slider = () => {
+    console.log("hello")
+    const { index } = this.state
+    if (index/100 < carousels.length-1) {
+      this.setState({index: index + 100})
+    } else {
+      this.setState({index: 0})
+    }
+  }
+
   render() {
+    const {index} = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div
+        className="slider"
+        style={{ 
+          transform: `translateX(-${index}%)`,
+          transition: `transform ${index ? 500 : 0}ms`,
+        }}
+      >
+        {carousels.map(index => 
+          <div className="slider-container">
+            <img src={index} alt="carousel_Image" className="responsive" />
+          </div>)
+        }
       </div>
-    );
+    )
   }
 }
 
